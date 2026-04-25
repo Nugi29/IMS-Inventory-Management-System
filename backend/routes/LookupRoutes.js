920385f79a331a141e8936ae486fab2172edb670
isharaudayamali <isharaudayamali220@gmail.com>
2026-04-25 21:33:43 +0530

    feat: Refactor LookupRoutes and supplierRoutes for improved organization and remove unused itemRoutes

--- backend/routes/LookupRoutes.js
+++ backend/routes/LookupRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const { getAllUserRoles, getAllUserStatuses, getAllCategories, getAllItemStatuses, getAllPoStatuses, getAllSuppliers, getAllUsers } = require('../controllers/LookupController');
-const { route } = require('./PurchaseOrderRoutes');
-const { getAllUserRoles, getAllUserStatuses } = require('../controllers/LookupController');
+const {
+    getAllUserRoles,
+    getAllUserStatuses,
+    getAllCategories,
+    getAllItemStatuses,
+    getAllPoStatuses,
+    getAllSuppliers,
+    getAllUsers,
+} = require('../controllers/LookupController');
 
 const router = express.Router();
 
@@ -14,9 +20,5 @@ router.get('/get-all-item-statuses', getAllItemStatuses);
 router.get('/get-all-po-statuses', getAllPoStatuses);
 router.get('/get-all-suppliers', getAllSuppliers);
 router.get('/get-all-users', getAllUsers);
-
-
-
-
 
 module.exports = router;
--- backend/routes/supplierRoutes.js
+++ backend/routes/supplierRoutes.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const { createSupplier, getAllSuppliers, getSupplierById, getSupplierByName, updateSupplier, deleteSupplier } = require('../controllers/SupplierController');
-const { createSupplier, getAllSuppliers, getSupplierById, getSupplierByName, updateSupplier, deleteSupplier } = require('../controllers/Supplier');
 const authUser = require('../middlewares/AuthUser');
 
 const router = express.Router();
 
 router.post('/', authUser, authUser.requireAdmin, createSupplier);
 router.get('/', authUser, getAllSuppliers);
+router.get('/search', authUser, getSupplierByName);
 router.get('/search/:name', authUser, getSupplierByName);
 router.get('/:id', authUser, getSupplierById);
-router.get('/search', authUser, getSupplierByName);
 router.put('/update/:id', authUser, authUser.requireAdmin, updateSupplier);
 router.delete('/delete/:id', authUser, authUser.requireAdmin, deleteSupplier);
 router.put('/update-supplier/:id', authUser, authUser.requireAdmin, updateSupplier);
 router.put('/:id', authUser, authUser.requireAdmin, updateSupplier);
 router.delete('/delete-supplier/:id', authUser, authUser.requireAdmin, deleteSupplier);
-router.put('/:id', authUser, authUser.requireAdmin, updateSupplier);
 router.delete('/:id', authUser, authUser.requireAdmin, deleteSupplier);
 
 module.exports = router;
